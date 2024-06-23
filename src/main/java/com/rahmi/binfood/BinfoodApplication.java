b609c0a229303564e63d9d26757736e4f26c6395
liecy <rahmianisa27@gmail.com>
2024-06-23 22:52:57 +0700

    fixxing errrorr

--- src/main/java/com/rahmi/binfood/BinfoodApplication.java
+++ src/main/java/com/rahmi/binfood/BinfoodApplication.java
@@ -1,6 +1,8 @@
 package com.rahmi.binfood;
 
+import jakarta.annotation.PostConstruct;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -12,8 +14,24 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication
 public class BinfoodApplication {
 
+	@Value("${spring.datasource.url}")
+	private String datasourceUrl;
+
+	@Value("${spring.datasource.username}")
+	private String datasourceUsername;
+
+	@Value("${spring.datasource.password}")
+	private String datasourcePassword;
+
 	public static void main(String[] args) {
 		SpringApplication.run(BinfoodApplication.class, args);
+	}
+
+	@PostConstruct
+	public void printDatasourceUrl() {
+		System.out.println("Datasource URL: " + datasourceUrl);
+		System.out.println("Datasource Username: " + datasourceUsername);
+		System.out.println("Datasource Password: " + datasourcePassword);
 	}
 
 	@Bean

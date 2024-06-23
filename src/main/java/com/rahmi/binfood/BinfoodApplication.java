29f36cdfa27e900604a495497bc39e6a29473d3f
liecy <rahmianisa27@gmail.com>
2024-06-23 21:38:03 +0700

    fix JPA configuration

--- src/main/java/com/rahmi/binfood/BinfoodApplication.java
+++ src/main/java/com/rahmi/binfood/BinfoodApplication.java
@@ -5,14 +5,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaAuditing
+@EnableJpaRepositories(basePackages = "com.rahmi.binfood.repository")
 @SpringBootApplication
 public class BinfoodApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BinfoodApplication.class, args);
-//		System.out.println("Hello World!");
 	}
 
 	@Bean
